4876b632fceffa8c9010e30b238edd8c2e0c9b0b
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2023-05-19 17:00:29 +0000

    new

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""Initial script for undesrtand flask"""
+   """Script that starts a Flask web application"""
+   from flask import Flask
+   app = Flask(__name__)
 
+   @app.route('/airbnb-onepage/', strict_slashes=False)
+   def hello():
+       return "Hello HBNB!"
 
-from flask import Flask
+   if __name__ == '__main__':
+       app.run(host='0.0.0.0', port=5000)
 
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
