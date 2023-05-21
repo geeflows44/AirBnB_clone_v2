e1ea002739d9b04edf51b67689b608818384f7fc
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2023-05-21 02:58:54 +0000

    new

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,14 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.url_map.strict_slashes = False
+
+@app.route("/airbnb-onepage/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 
-@app.route('/')
-def home_page():
-    """" Show a message when accessed """
-    return 'Hello HBNB!'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
